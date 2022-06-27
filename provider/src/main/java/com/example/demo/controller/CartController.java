be84bc97124cd9d78e1b20c64f1eedebab0b8abe
Marius <marius.hilleke@thoughtworks.com>
2022-06-27 07:36:58 +0200

    add type for provider-response

--- provider/src/main/java/com/example/demo/controller/CartController.java
+++ provider/src/main/java/com/example/demo/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Item;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
 
   @GetMapping("/items")
-  List<String> getItems(){
-    return List.of("A", "B", "C");
+  List<Item> getItems(){
+    return List.of(new Item("A"), new Item("B"), new Item("C"));
   }
 }
