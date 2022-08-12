8e4718ec71079072721566969d6dd359434d6cfb
Marius <marius.hilleke@thoughtworks.com>
2022-08-12 08:34:18 +0200

    add provider test for ui-contract

---
+++ storage-service/src/test/java/com/example/demo/contract/StorageUiProviderContractTest.java
@@ -0,0 +1,53 @@
+package com.example.demo.contract;
+
+import static org.mockito.Mockito.when;
+
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Consumer;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import com.example.demo.entity.Item;
+import com.example.demo.service.StorageService;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@Provider("StorageApplication")
+@Consumer("CartApplication")
+@PactFolder("contracts")
+@Tag("contractTest")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CartProviderContractTest {
+
+  @LocalServerPort
+  int port;
+
+  @MockBean
+  private StorageService storageService;
+
+  @BeforeEach
+  void setUp(PactVerificationContext context) {
+    context.setTarget(new HttpTestTarget("localhost", port));
+  }
+
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  void verifyPact(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+
+  @State("items exist")
+  void toItemsExistState() {
+    when(storageService.getItemsAvailable()).thenReturn(
+        Arrays.asList(new Item("A"), new Item("B"), new Item("C"))
+    );
+  }
+}
